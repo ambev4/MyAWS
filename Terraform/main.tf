cee5e433219100cdc2ad023bfcdf3e1ed311efed
Lucas Barbosa <dockerjob@gmail.com>
2024-09-19 17:10:36 -0300

    Criação da VM no terraform e import da local settings no arquivo de settings.py

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -72,3 +72,30 @@ resource "aws_internet_gateway" "internet_gateway" {
     Name = "igw-01"
   }
 }
+
+# Terraform Data Block - To Lookup Latest Ubuntu 24.04 AMI Image
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+# Terraform Resource Block - To Build EC2 instance in Public Subnet
+resource "aws_instance" "web_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnets["sb-app-01"].id
+  tags = {
+    Name = "ec2-app-01"
+  }
+}
