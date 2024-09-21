1f1f828d3d34c6704ee9f6821c3641a3f6c0ff79
Lucas Barbosa <dockerjob@gmail.com>
2024-09-21 15:25:34 -0300

    Atualização do layout de grid dos posts, reformulação do arquivo postblock.tml, criação de novo dicionário para data de texto

--- Python/port_lucas/data.py
+++ Python/port_lucas/data.py
@@ -1,12 +1,6 @@
-posts = [
-    {
-        "id": 1,
-        "body": "Descrição profissional" + ("-" * 200),
-        "img": "global/static/global/images/foto1.jpg"
-    },
-    {
-        "id": 2,
-        "body": "Descrição do ambiente" + ("-" * 200),
-        "img": "global/static/global/images/foto2.png"
-    }
-]
+posts = {
+        "descricao_pessoal": "1",
+        "descricao_ambiente": "",
+        "descricao_terraform": "",
+        "descricao_ansible": ""
+}
--- Python/port_lucas/views.py
+++ Python/port_lucas/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from port_lucas import data
 
 def home(request):
-    context = {
+    text = {
         'posts': data.posts
     }
-    return render(request, 'port_lucas/index.html', context)
+    return render(request, 'port_lucas/index.html', text)
