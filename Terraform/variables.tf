8444e472d9d27f15ffc8bd1bf9c9bbea47002d5b
Lucas Barbosa <dockerjob@gmail.com>
2024-10-02 20:50:17 -0300

    Conclusão dos comandos para configuração e criação do webserver, uso do S3 bucket para repositorio de arquivos estáticos.

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -26,7 +26,7 @@ variable "web_server_cmds" {
   default = [
     "sudo apt update && sudo apt upgrade -y",
     "sudo apt autoremove -y",
-    "sudo apt install python3.12 python3.12-venv nginx git -y",
+    "sudo apt install python3.12 nginx git python3.12-venv -y",
     "git config --global user.name 'Lucas Barbosa'",
     "git config --global user.email 'dockerjob@gmail.com'",
     "git config --global init.defaultBranch master",
@@ -36,13 +36,28 @@ variable "web_server_cmds" {
     "git init",
     "git add .",
     "git commit -m 'Initial'",
-    # Testar esses comandos abaixo
     "python3.12 -m venv ~/myawsapp/Python/venv",
+    "wget https://ambev4-dados.s3.amazonaws.com/myaws-static-repo/local_settings.py -P ~/myawsapp/Python/MyAWS/",
     ". ~/myawsapp/Python/venv/bin/activate",
     "pip install --upgrade pip",
-    "pip install django",
-    "pip install pillow",
-    "pip install gunicorn",
-    "pip install psycopg"
+    "pip install django gunicorn",
+    "python ~/myawsapp/Python/manage.py migrate",
+    "python ~/myawsapp/Python/manage.py collectstatic",
+    "mkdir ~/myawsapp/Python/static_server/global/static/global/images",
+    "wget https://ambev4-dados.s3.amazonaws.com/myaws-static-repo/aws-selo.png -P ~/myawsapp/Python/static_server/global/static/global/images",
+    "wget https://ambev4-dados.s3.amazonaws.com/myaws-static-repo/foto1.jpg -P ~/myawsapp/Python/static_server/global/static/global/images",
+    "wget https://ambev4-dados.s3.amazonaws.com/myaws-static-repo/Diagrama-ambiente-aws.png -P ~/myawsapp/Python/static_server/global/static/global/images",
+    "sudo wget https://ambev4-dados.s3.amazonaws.com/myaws-static-repo/myaws.socket -P /etc/systemd/system/",
+    "sudo wget https://ambev4-dados.s3.amazonaws.com/myaws-static-repo/myaws.service -P /etc/systemd/system/",
+    "sudo systemctl daemon-reload",
+    "sudo systemctl start myaws.socket",
+    "sudo systemctl enable myaws.socket",
+    "sudo systemctl start myaws.service",
+    "sudo systemctl enable myaws.service",
+    "sudo wget https://ambev4-dados.s3.amazonaws.com/myaws-static-repo/myaws -P /etc/nginx/sites-available/",
+    "sudo rm -f /etc/nginx/sites-enabled/default",
+    "sudo ln -s /etc/nginx/sites-available/myaws /etc/nginx/sites-enabled/",
+    "sudo sed -i 's/www-data/ubuntu/g' /etc/nginx/nginx.conf",
+    "sudo systemctl restart nginx"
   ]
 }
